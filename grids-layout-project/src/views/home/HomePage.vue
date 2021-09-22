57a8feb9facbe81a5ab52b0b2323a7ada9550be3
frankkhiem <khiem.ngg@gmail.com>
2021-09-22 16:19:41 +0700

    Create grid layout base

---
+++ grids-layout-project/src/views/home/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div class="intro">
+      <h1>Vue Gridster</h1>
+    </div>
+    
+    <div class="grid-container">
+      <grid></grid>
+    </div>
+  </div>
+</template>
+<script>
+import Grid from '@/components/grid-layout/Grid.vue';
+
+export default {
+  components: {
+    Grid,
+  },
+
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+  h1 {
+    text-align: center;
+    margin-bottom: 50px;
+  }
+  .grid-container {
+    width: 80%;
+    margin: 20px auto;
+    border: 1px solid #777;
+  }
+</style>
--- k3mn_shop/resources/js/store/modules/index.js
+++
@@ -1,9 +0,0 @@
-import User from './user.module';
-import Modals from './modals.module';
-import Notification from './notification.module';
-
-export const modules = {
-  User,
-  Modals,
-  Notification,
-}
